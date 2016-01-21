e0981441cb6764017f3a319d669db55596947b82
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-21 15:53:38 +0000

    updated display type for dates and release date

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.EnterpriseServices;
 
 namespace MvcMovie.Models
 {
@@ -10,6 +12,10 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
