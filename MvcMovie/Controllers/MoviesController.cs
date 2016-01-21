4d39541a0bac093240f874cf1239be92b623df57
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-21 16:38:59 +0000

    added search and filter by genre functionality

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -15,9 +15,31 @@ namespace MvcMovie.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Index(string movieGenre, string searchString)
         {
-            return View(db.Movies.ToList());
+            var GenreList = new List<string>();
+
+            var GenreQuery = from d in db.Movies
+                orderby d.Genre
+                select d.Genre;
+
+            GenreList.AddRange(GenreQuery.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreList);
+
+            var movies = from m in db.Movies
+                select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
