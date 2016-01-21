292a111d530ecc2c476547074ea1a582555a194f
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-21 12:50:03 +0000

    added view for helloworld controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -6,9 +6,9 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/ 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
         // GET: /HelloWorld/Welcome/ 
         public string Welcome(string name, int ID = 1)
